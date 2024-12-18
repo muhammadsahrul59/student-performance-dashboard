0d82936db16a4cd5e778462a0ff453f59d1179a8
MUHAMMAD SAHRUL <m.sahrul59@gmail.com>
2024-12-18 20:49:19 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,81 @@
+<<<<<<< HEAD
+asdwwasdw
+=======
+# Proyek Akhir: Menyelesaikan Permasalahan Dropout Mahasiswa di Kiyut Institute
+
+## Business Understanding
+
+Kiyut Institute merupakan salah satu institusi pendidikan perguruan yang telah berdiri sejak tahun 2000. Hingga saat ini ia telah mencetak banyak lulusan dengan reputasi yang sangat baik. Akan tetapi, terdapat banyak juga siswa yang tidak menyelesaikan pendidikannya alias dropout.
+
+Jumlah dropout yang tinggi ini tentunya menjadi salah satu masalah yang besar untuk sebuah institusi pendidikan. Oleh karena itu, Kiyut Institute ingin mendeteksi secepat mungkin siswa yang mungkin akan melakukan dropout sehingga dapat diberi bimbingan khusus.
+
+### Permasalahan Bisnis
+
+- Bagaimana mengidentifikasi karakteristik mahasiswa yang memiliki risiko tinggi untuk dropout?
+- Apakah terdapat pola dropout berdasarkan gender, usia, status pernikahan, dan jurusan?
+- Jurusan dan kelompok usia mana yang memiliki tingkat dropout tertinggi?
+
+### Cakupan Proyek
+
+1. Menganalisis data performa mahasiswa dan mencari penyebab dropout.
+2. Membuat dashboard performa mahasiswa untuk menampilkan statistik terkait dropout dan memberikan wawasan yang lebih baik.
+3. Menyediakan rekomendasi berbasis machine learning untuk mengurangi tingkat dropout.
+
+### Persiapan
+
+Sumber data: [Student Perfomance Dataset](https://archive.ics.uci.edu/dataset/697/predict+students+dropout+and+academic+success)
+
+Setup environment:
+
+```
+#Instal virtual env with conda
+conda create --name student_performance python=3.9
+
+#activate virtual env
+conda activate student_performance
+
+# Install necessary packages
+pip install -r requirements.txt
+
+# Run the Streamlit app
+streamlit run app.py
+```
+
+## Business Dashboard
+
+Dashboard ini menampilkan berbagai visualisasi terkait performa mahasiswa yang mencakup jumlah total mahasiswa, tingkat dropout, distribusi dropout berdasarkan usia, jenis kelamin, status pernikahan, serta program studi. Dashboard ini berfungsi sebagai alat bagi manajemen untuk memahami pola dropout mahasiswa, mengidentifikasi kelompok usia dan jurusan dengan tingkat dropout tertinggi, serta menganalisis tren berdasarkan gender dan status sosial untuk mendukung pengambilan keputusan yang lebih tepat.
+
+Kiyut Institute Student Performance Dashboard bisa diliat disini: [Tableau Public Dashboard](https://public.tableau.com/views/student-performance/KiyutInstitute?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link)
+
+## Menjalankan Sistem Machine Learning
+
+Untuk menjalankan machine learning prototype, ikuti langkah berikut:
+
+```
+# Clone the repository
+git clone <repository-url>
+
+# Install necessary packages
+pip install -r requirements.txt
+
+# Run the Streamlit app
+streamlit run app.py
+```
+
+Berikut adalah machine learning prototype dari Kiyut Institute : [Kiyut Institute Student Dropout Prediction App]()
+
+## Conclusion
+
+Dashboard ini menampilkan berbagai visualisasi terkait performa mahasiswa dengan fokus pada analisis dropout. Tingginya angka **dropout sebesar 32.55%** dari total **3,174 mahasiswa** mengindikasikan masalah serius yang perlu ditangani. Berdasarkan visualisasi, dropout tertinggi terjadi pada m**ahasiswa usia di bawah 20 tahun (392 kasus)** dan **20-29 tahun (468 kasus)**, menunjukkan bahwa mahasiswa muda lebih rentan mengalami dropout. Dari segi gender, **laki-laki** (51%) sedikit lebih mendominasi dibanding **perempuan** (49%), sedangkan dari status pernikahan, mahasiswa **single** menyumbang dropout tertinggi (860 kasus). Dropout juga paling banyak terjadi di program studi seperti **Management (kelas malam)** , **Management reguler**, dan **Nursing**, yang masing-masing mencatat lebih dari **100 kasus**.
+
+Tren ini mengarah pada perlunya perhatian khusus terhadap mahasiswa muda, program studi tertentu, serta faktor sosial seperti status pernikahan dalam upaya mengurangi angka dropout melalui intervensi yang tepat seperti dukungan akademik, konseling, dan peningkatan fasilitas belajar.
+
+### Rekomendasi Action Items
+
+Berikut adalah rekomendasi action items yang harus diterapkan oleh Kiyut Institute agar meningkatkan lulusan dengan reputasi yang baik:
+
+- **Bimbingan Akademik Intensif**: Targetkan mahasiswa muda (di bawah 20) agar mendapatkan dukungan tambahan.
+- **Konsultasi Psikologis**: Fokus pada mahasiswa single yang berisiko mengalami dropout.
+- **Evaluasi Jurusan dengan Tingkat Dropout Tinggi**: Manajemen dan Nursing perlu evaluasi lebih mendalam.
+- **Penerapan Early Warning System (EWS)**: Identifikasi mahasiswa dengan performa rendah secara dini menggunakan model prediktif.
+>>>>>>> b67b746 (first commit)
---
+++ app.py
@@ -0,0 +1,132 @@
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder
+from joblib import load
+import streamlit as st
+
+# Import Model
+model = load("model/xbcmodel.joblib")
+
+columns_to_scale = [
+    "Curricular units 1st sem (approved)",
+    "Curricular units 1st sem (grade)",
+    "Curricular units 2nd sem (approved)",
+    "Curricular units 2nd sem (grade)",
+    "Age at enrollment",
+]
+
+feature = ["Debtor", "Scholarship holder", "Tuition fees up to date"]
+
+
+# Function to preprocess input data
+def preprocess_data(df):
+    processed_df = df.copy()
+
+    # Perform label encoding for categorical columns
+    categorical_cols = [
+        "Debtor",
+        "Scholarship holder",
+        "Tuition fees up to date",
+        "Application mode",
+    ]
+    label_encoder = LabelEncoder()
+    for col in categorical_cols:
+        if col in processed_df.columns:
+            processed_df[col] = label_encoder.fit_transform(processed_df[col])
+
+    # Perform Min-Max scaling for all columns
+    mm_scaler = MinMaxScaler()
+    processed_df[processed_df.columns] = mm_scaler.fit_transform(processed_df)
+
+    return processed_df
+
+
+def preprocessor(input_df):
+    # Preprocess categorical and numerical columns separately
+    input_df = preprocess_data(input_df)
+    return input_df
+
+
+# Streamlit app
+def main():
+    col1, col2 = st.columns([1, 2])
+    with col1:
+        st.image(
+            "https://github.com/muhammadsahrul59/student-dropout-app/blob/main/images/logo-cute.png?raw=true",
+            width=130,
+        )
+    with col2:
+        st.header("Kiyut Institute")
+        st.header("Student Dropout Predictor")
+
+    # Input form
+    st.subheader("Masukkan Informasi Mahasiswa:")
+
+    input_data = {}  # Dictionary to store user input data
+    col1, col2 = st.columns(2)  # Split the interface into two columns
+
+    with col1:
+        # Collect user inputs for country and some financial indicators
+        input_data["Debtor"] = st.radio("Pilih Debtor:", ["Tidak", "Ya"])
+        input_data["Scholarship holder"] = st.radio(
+            "Pilih Scholarship holder:", ["Tidak", "Ya"]
+        )
+        input_data["Tuition fees up to date"] = st.radio(
+            "Pilih Tuition fees up to date:", ["Tidak", "Ya"]
+        )
+        input_data["Application mode"] = st.selectbox(
+            "Application mode:",
+            [
+                "2nd phase - general contingent" "International student (bachelor)",
+                "1st phase - general contingent",
+                "Over 23 years old",
+                "3rd phase - general contingent",
+                "Short cycle diploma holders",
+                "Technological specialization diploma holders",
+                "Change of institution/course",
+                "Change of course",
+                "Holders of other higher courses",
+                "Transfer",
+                "1st phase - special contingent (Madeira Island)",
+                "1st phase - special contingent (Azores Island)",
+                "Ordinance No. 612/93",
+                "Ordinance No. 854-B/99",
+                "Change of institution/course (International)",
+                "Ordinance No. 533-A/99, item b2 (Different Plan)",
+                "Ordinance No. 533-A/99, item b3 (Other Institution)",
+            ],
+        )
+
+    with col2:
+        input_data["Age at enrollment"] = st.number_input("Umur", step=1)
+        input_data["Curricular units 1st sem (approved)"] = st.number_input(
+            "Menyetujui Mata Kuliah Semester Pertama: ", step=1
+        )
+        input_data["Curricular units 1st sem (grade)"] = st.number_input(
+            "Nilai Semester 1: ", step=1
+        )
+        input_data["Curricular units 2nd sem (approved)"] = st.number_input(
+            "Menyetujui Unit Mata Kuliah Semester Pertama: ", step=1
+        )
+        input_data["Curricular units 2nd sem (grade)"] = st.number_input(
+            "Nilai Semester 2: ", step=1
+        )
+        # inputs = {}
+
+        # for column in columns_to_scale:
+        # inputs[column] = st.number_input(f"{column}:", value=0.0, min_value=0.0, max_value=100.0, step=1.0)
+
+    input_df = pd.DataFrame([input_data])  # Convert collected data into a DataFrame
+
+    if st.button("Prediksi"):  # When the 'Predict' button is clicked
+        final_df = preprocessor(input_df)  # Preprocess the collected data
+        prediction = model.predict(final_df)[0]  # Use the model to predict the outcome
+
+        # Display the prediction result
+        if prediction == 1:
+            st.write("Graduate")
+        elif prediction == 0:
+            st.write("Dropout")
+
+
+if __name__ == "__main__":
+    main()
